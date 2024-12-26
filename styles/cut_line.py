0208e05289e491f51b5e6f5cefde4c5dc70aedd8
isinvon <2021469084@qq.com>
2024-12-26 15:23:30 +0800

    feat(styles): 添加文本居中分割线函数

--- styles/cut_line.py
+++ styles/cut_line.py
@@ -4,3 +4,10 @@ from colorama import Fore, Back, Style
 def cut_line():
     """ 分割线 """
     print(Fore.WHITE + Style.BRIGHT + "-" * 75 + Style.RESET_ALL)
+
+
+def cut_line_msg(msg):
+    """ 文本居中分割线 """
+    msg_length = len(msg)
+    side_length = (75 - msg_length) // 2
+    print(Fore.WHITE + Style.BRIGHT + "-" * side_length + msg + "-" * side_length + Style.RESET_ALL)
