8b3c31822bdc3abf21cdd794276bf8373d091dbb
isinvon <2021469084@qq.com>
2024-12-26 13:45:58 +0800

    fix(utils): 优化 logUtils 日志记录模块,避免日志重复写入
    - 添加全局 logger 变量，避免重复初始化
    - 在 setup_logger 函数中增加判断，防止重复添加处理器
    - 优化日志配置流程，提高代码可读性
    - 修复 logging_no_print_warning 函数中的缩进问题

--- utils/logUtils.py
+++ utils/logUtils.py
@@ -1,9 +1,16 @@
+# logUtils.py
 import logging
 import os
 from logging.handlers import TimedRotatingFileHandler
 from datetime import datetime
 
+logger = None
+
 def setup_logger():
+    global logger
+    if logger is not None:
+        return logger
+
     # 获取当前日期
     current_date = datetime.now()
     year_month = current_date.strftime('%Y-%m')
@@ -21,13 +28,16 @@ def setup_logger():
     logger = logging.getLogger('my_logger')
     logger.setLevel(logging.DEBUG)
     
-    # 创建TimedRotatingFileHandler
-    handler = TimedRotatingFileHandler(log_file_path, when='midnight', interval=1, backupCount=30)
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    
-    # 将处理器添加到日志记录器
-    logger.addHandler(handler)
+    # 检查是否已有处理器
+    if not logger.handlers:
+        # 创建TimedRotatingFileHandler
+        handler = TimedRotatingFileHandler(log_file_path, when='midnight', interval=1, backupCount=30)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        
+        # 将处理器添加到日志记录器
+        logger.addHandler(handler)
+
     return logger
 
 # -----------------------------------------------------
@@ -59,5 +69,5 @@ def logging_no_print_error(message):
     logger.error(message)
     
 def logging_no_print_warning(message):
-        logger = setup_logger()
-        logger.warning(message)
+    logger = setup_logger()
+    logger.warning(message)
