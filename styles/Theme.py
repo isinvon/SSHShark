c44f5e15a70cbb178fbf2391902e64bf224bf8ff
isinvon <2021469084@qq.com>
2024-12-27 14:56:13 +0800

    fix(styles): Theme.py从Back改为Fore

--- styles/Theme.py
+++ styles/Theme.py
@@ -1,29 +1,30 @@
 from enum import Enum
-from colorama import Back
+
+from colorama import Fore
 
 
 class Theme(Enum):
     """ 主题颜色 """
 
     # background
-    black_back = Back.BLACK  # \033[40m
-    red_back = Back.RED  # \033[41m
-    green_back = Back.GREEN  # \033[42m
-    yellow_back = Back.YELLOW  # \033[43m
-    blue_back = Back.BLUE  # \033[44m
-    magenta_back = Back.MAGENTA  # \033[45m
-    cyan_back = Back.CYAN  # \033[46m
-    white_back = Back.WHITE  # \033[47m
+    black_back = Fore.BLACK  # \033[40m
+    red_back = Fore.RED  # \033[41m
+    green_back = Fore.GREEN  # \033[42m
+    yellow_back = Fore.YELLOW  # \033[43m
+    blue_back = Fore.BLUE  # \033[44m
+    magenta_back = Fore.MAGENTA  # \033[45m
+    cyan_back = Fore.CYAN  # \033[46m
+    white_back = Fore.WHITE  # \033[47m
 
     # background - height light
-    lightblack_back = Back.LIGHTBLACK_EX  # \033[100m
-    lightred_back = Back.LIGHTRED_EX  # \033[101m
-    lightgreen_back = Back.LIGHTGREEN_EX  # \033[102m
-    lightyellow_back = Back.LIGHTYELLOW_EX  # \033[103m
-    lightblue_back = Back.LIGHTBLUE_EX  # \033[104m
-    lightmagenta_back = Back.LIGHTMAGENTA_EX  # \033[105m
-    lightcyan_back = Back.LIGHTCYAN_EX  # \033[106m
-    lightwhite_back = Back.LIGHTWHITE_EX  # \033[107m
+    lightblack_back = Fore.LIGHTBLACK_EX  # \033[100m
+    lightred_back = Fore.LIGHTRED_EX  # \033[101m
+    lightgreen_back = Fore.LIGHTGREEN_EX  # \033[102m
+    lightyellow_back = Fore.LIGHTYELLOW_EX  # \033[103m
+    lightblue_back = Fore.LIGHTBLUE_EX  # \033[104m
+    lightmagenta_back = Fore.LIGHTMAGENTA_EX  # \033[105m
+    lightcyan_back = Fore.LIGHTCYAN_EX  # \033[106m
+    lightwhite_back = Fore.LIGHTWHITE_EX  # \033[107m
 
     def __str__(self) -> str:
         return self.value
