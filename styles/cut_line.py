d7502342f5b461431351ddfc0d23116f308d97a4
isinvon <2021469084@qq.com>
2024-12-27 18:09:09 +0800

    style(cut_line): 更新分割线样式
    - 将分割线字符从 "-" 改为 "═"，提升视觉效果
    - 调整分割线长度
    - 在文本居中分割线中添加空格

--- styles/cut_line.py
+++ styles/cut_line.py
@@ -3,11 +3,11 @@ from colorama import Fore, Back, Style
 
 def cut_line():
     """ 分割线 """
-    print(Fore.WHITE + Style.BRIGHT + "-" * 75 + Style.RESET_ALL)
+    print(Fore.WHITE + Style.BRIGHT + "═" * 78 + Style.RESET_ALL)
 
 
 def cut_line_msg(msg):
     """ 文本居中分割线 """
     msg_length = len(msg)
-    side_length = (75 - msg_length) // 2
-    print(Fore.WHITE + Style.BRIGHT + "-" * side_length + msg + "-" * side_length + Style.RESET_ALL)
+    side_length = (78 - msg_length) // 2
+    print(Fore.WHITE + Style.BRIGHT + "═" * side_length + " " + msg + " " + "═" * side_length + Style.RESET_ALL)
