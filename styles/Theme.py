3c56f1ebf20120670c7486a205f6940120745e4c
isinvon <2021469084@qq.com>
2024-12-27 15:06:46 +0800

    fix(styles): 重命名主题颜色属性
    - 将 "black_back" 等属性名简化为 "black"，移除冗余的 "_back" 后缀

--- styles/Theme.py
+++ styles/Theme.py
@@ -7,24 +7,24 @@ class Theme(Enum):
     """ 主题颜色 """
 
     # background
-    black_back = Fore.BLACK  # \033[40m
-    red_back = Fore.RED  # \033[41m
-    green_back = Fore.GREEN  # \033[42m
-    yellow_back = Fore.YELLOW  # \033[43m
-    blue_back = Fore.BLUE  # \033[44m
-    magenta_back = Fore.MAGENTA  # \033[45m
-    cyan_back = Fore.CYAN  # \033[46m
-    white_back = Fore.WHITE  # \033[47m
+    black = Fore.BLACK  # \033[40m
+    red = Fore.RED  # \033[41m
+    green = Fore.GREEN  # \033[42m
+    yellow = Fore.YELLOW  # \033[43m
+    blue = Fore.BLUE  # \033[44m
+    magenta = Fore.MAGENTA  # \033[45m
+    cyan = Fore.CYAN  # \033[46m
+    white = Fore.WHITE  # \033[47m
 
     # background - height light
-    lightblack_back = Fore.LIGHTBLACK_EX  # \033[100m
-    lightred_back = Fore.LIGHTRED_EX  # \033[101m
-    lightgreen_back = Fore.LIGHTGREEN_EX  # \033[102m
-    lightyellow_back = Fore.LIGHTYELLOW_EX  # \033[103m
-    lightblue_back = Fore.LIGHTBLUE_EX  # \033[104m
-    lightmagenta_back = Fore.LIGHTMAGENTA_EX  # \033[105m
-    lightcyan_back = Fore.LIGHTCYAN_EX  # \033[106m
-    lightwhite_back = Fore.LIGHTWHITE_EX  # \033[107m
+    lightblack = Fore.LIGHTBLACK_EX  # \033[100m
+    lightred = Fore.LIGHTRED_EX  # \033[101m
+    lightgreen = Fore.LIGHTGREEN_EX  # \033[102m
+    lightyellow = Fore.LIGHTYELLOW_EX  # \033[103m
+    lightblue = Fore.LIGHTBLUE_EX  # \033[104m
+    lightmagenta = Fore.LIGHTMAGENTA_EX  # \033[105m
+    lightcyan = Fore.LIGHTCYAN_EX  # \033[106m
+    lightwhite = Fore.LIGHTWHITE_EX  # \033[107m
 
     def __str__(self) -> str:
         return self.value
