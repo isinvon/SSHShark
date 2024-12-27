23e1332e0090fedf1c193099c1c1cdf56a0a132b
isinvon <2021469084@qq.com>
2024-12-27 18:22:13 +0800

    refactor(styles): 重构分割线样式以支持主题
    - 引入 Theme 类以获取当前主题颜色
    - 将分割线颜色改为根据当前主题配置中获取

--- styles/cut_line.py
+++ styles/cut_line.py
@@ -1,13 +1,16 @@
 from colorama import Fore, Back, Style
 
+from styles.Theme import Theme
+
 
 def cut_line():
     """ 分割线 """
-    print(Fore.WHITE + Style.BRIGHT + "═" * 78 + Style.RESET_ALL)
+    print(Theme.get_theme() + Style.BRIGHT + "═" * 78 + Style.RESET_ALL)
 
 
 def cut_line_msg(msg):
     """ 文本居中分割线 """
     msg_length = len(msg)
     side_length = (78 - msg_length) // 2
-    print(Fore.WHITE + Style.BRIGHT + "═" * side_length + " " + msg + " " + "═" * side_length + Style.RESET_ALL)
+    print(Theme.get_theme() + Style.BRIGHT + "═" * side_length +
+          " " + msg + " " + "═" * side_length + Style.RESET_ALL)
